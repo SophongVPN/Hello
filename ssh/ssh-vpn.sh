efc8605ee4e82b28bc2a263967a6da9d8097db06
SophongVPN <83055091+SophongVPN@users.noreply.github.com>
2022-07-23 07:45:43 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -213,7 +213,7 @@ RUN=yes
 # systemd users: don't forget to modify /lib/systemd/system/sslh.service
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --pidfile /var/run/sslh/sslh.pid -n"
+DAEMON_OPTS="--user sslh --listen 0.0.0.0:443 --listen 0.0.0.0:8243 --listen 0.0.0.0:9443 --listen 0.0.0.0:2053 --listen 0.0.0.0:2083 --listen 0.0.0.0:2087 --listen 0.0.0.0:2096 --listen 0.0.0.0:8443 --ssl 127.0.0.1:777 --ssh 127.0.0.1:109 --openvpn 127.0.0.1:1194 --http 127.0.0.1:8880 --http 127.0.0.1:8280 --http 127.0.0.1:80 --http 127.0.0.1:2052 --http 127.0.0.1:2082 --http 127.0.0.1:2086 --http 127.0.0.1:2095 --pidfile /var/run/sslh/sslh.pid -n"
 
 END
 
